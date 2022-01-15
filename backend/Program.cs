39ccb9611c4126d2a96cb201eb1ed750ee89c21a
Vlad <MadVlad1715@gmail.com>
2022-01-15 11:37:38 +0200

    Connect database to app

--- backend/Program.cs
+++ backend/Program.cs
@@ -1,4 +1,38 @@
+using DocsGen.Models;
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<UniversityContext>(options =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        string connection = builder.Configuration.GetConnectionString("DefaultConnection");
+        options
+        .UseNpgsql(connection)
+        .EnableSensitiveDataLogging();
+    }
+    else
+    {
+        var databaseUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+        if (databaseUrl == null) throw new Exception("Environment variable DATABASE_URL is null.");
+
+        var databaseUri = new Uri(databaseUrl!);
+        var userInfo = databaseUri.UserInfo.Split(':');
+
+        var conStrBuilder = new NpgsqlConnectionStringBuilder
+        {
+            Host = databaseUri.Host,
+            Port = databaseUri.Port,
+            Username = userInfo[0],
+            Password = userInfo[1],
+            Database = databaseUri.LocalPath.TrimStart('/')
+        };
+
+        options.UseNpgsql(conStrBuilder.ToString());
+    }
+});
 
 builder.Services.AddControllers();
 
@@ -28,5 +62,12 @@ app.UseWhen(
                 endpoints.MapFallbackToFile("index.html");
             });
     });
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<UniversityContext>();
+
+    db.Database.Migrate();
+}
 
 app.Run();
