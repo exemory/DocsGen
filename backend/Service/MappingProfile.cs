c84af3c8b92d8f6ddc41877b2c9d66786825ab5b
Vlad <MadVlad1715@gmail.com>
2022-01-29 20:52:56 +0200

    Add service tier
    Create EntityService and MappingProfile classes

---
+++ backend/Service/MappingProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Core.DTOs;
+using Core.Entities;
+
+namespace Service
+{
+    internal class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Guarantor, GuarantorDTO>();
+            CreateMap<GuarantorDTO, Guarantor>();
+        }
+    }
+}
---
+++ backend/Service/Services/EntityService.cs
@@ -0,0 +1,82 @@
+﻿using AutoMapper;
+using Core;
+using Core.DTOs.Base;
+using Core.Entities.Base;
+using Core.Exceptions;
+using Core.Services;
+
+namespace Service.Services
+{
+    public class EntityService<TEntity, TDTO> : IEntityService<TDTO>
+        where TEntity : Entity, new()
+        where TDTO : DTO
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public EntityService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<TDTO> GetById(int id)
+        {
+            var entity = await _unitOfWork.Repository<TEntity>().GetById(id);
+            if (entity == null)
+            {
+                throw new EntityNotFoundException("Entity doesn't exist in the repository.", typeof(TEntity));
+            }
+
+            var dto = _mapper.Map<TEntity, TDTO>(entity);
+            return dto;
+        }
+
+        public async Task<IEnumerable<TDTO>> GetAll()
+        {
+            var entities = await _unitOfWork.Repository<TEntity>().GetAll();
+            var dtos = _mapper.Map<IEnumerable<TEntity>, IEnumerable<TDTO>>(entities);
+            return dtos;
+        }
+
+        public async Task<TDTO> Add(TDTO dto)
+        {
+            var entity = _mapper.Map<TDTO, TEntity>(dto);
+            entity.Id = default;
+
+            _unitOfWork.Repository<TEntity>().Add(entity);
+
+            await _unitOfWork.Save();
+
+            dto = _mapper.Map<TEntity, TDTO>(entity);
+            return dto;
+        }
+
+        public async Task Update(TDTO dto)
+        {
+            var entity = _mapper.Map<TDTO, TEntity>(dto);
+
+            var exists = await _unitOfWork.Repository<TEntity>().Exists(entity);
+            if (!exists)
+            {
+                throw new EntityNotFoundException("Entity doesn't exist in the repository.", typeof(TEntity));
+            }
+
+            _unitOfWork.Repository<TEntity>().Update(entity);
+            await _unitOfWork.Save();
+        }
+
+        public async Task Delete(int id)
+        {
+            var exists = await _unitOfWork.Repository<TEntity>().Exists(id);
+            if (!exists)
+            {
+                throw new EntityNotFoundException("Entity doesn't exist in the repository.", typeof(TEntity));
+            }
+
+            _unitOfWork.Repository<TEntity>().Delete(id);
+
+            await _unitOfWork.Save();
+        }
+    }
+}
